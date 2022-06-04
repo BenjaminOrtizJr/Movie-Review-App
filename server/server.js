993b67e839f1169116c848e8788e194f2492b7e8
BenjaminOrtizJr <bortizjr84@outlook.com>
2022-06-04 13:40:02 -0400

    backend completed

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import cors from 'cors'
+import Morgan from 'morgan'
 import movies from './api/movies.route.js'
+import morgan from 'morgan'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(morgan('dev'))
 
 app.use("/api/v1/movies", movies)
 app.use('*', (req, res) => {
